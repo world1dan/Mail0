f3a6c698c3b1c632674f2b519badfe128fac057d
James Haworth <james@jameshaworth.dev>
2025-02-05 17:04:57 +0000

    ui: improve theme toggle post-select to remove outline

--- components/theme/mode-toggle.tsx
+++ components/theme/mode-toggle.tsx
@@ -19,7 +19,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <SidebarMenuButton>
+        <SidebarMenuButton className="focus-visible:ring-0 focus-visible:ring-offset-0">
           <Sun className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
