cfd4c0f956f300a8f76b2c31da2c79657f710d31
Nizzy <nizabizaher@gmail.com>
2025-02-06 21:18:46 -0500

    posthog

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -2,12 +2,23 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { PostHogProvider } from "posthog-js/react";
+import posthog from "posthog-js";
 import * as React from "react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
 
 export function Providers({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <NextThemesProvider {...props}>
-      <SidebarProvider>{children}</SidebarProvider>
+      <PostHogProvider client={posthog}>
+        <SidebarProvider>{children}</SidebarProvider>
+      </PostHogProvider>
     </NextThemesProvider>
   );
 }
