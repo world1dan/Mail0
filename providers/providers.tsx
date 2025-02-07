7fe3807ac598470a98173e1d5aa6c0657b4aedf8
Rahul Mishra <blankparticle@gmail.com>
2025-02-07 10:25:33 +0530

    fix: load posthog only if key is present to prevent error in dev (#40)

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -6,8 +6,8 @@ import { PostHogProvider } from "posthog-js/react";
 import posthog from "posthog-js";
 import * as React from "react";
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
