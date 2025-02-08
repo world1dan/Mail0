7f1dbdfcf115196f9f182becc6d9a731f1b6d2e6
Ahmet Kilinc <akx9@icloud.com>
2025-02-08 23:35:43 +0000

    fix: broken screen of whole app (#106)

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -2,7 +2,7 @@ import { AppSidebar } from "@/components/ui/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex h-screen w-screen overflow-hidden">
       <AppSidebar />
       {children}
     </div>
