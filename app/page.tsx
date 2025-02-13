d28db9a8817943f5051559a402fe8b0002752dcb
Nizzy <nizabizaher@gmail.com>
2025-02-13 00:45:09 -0500

    google oauth reqs

--- app/page.tsx
+++ app/page.tsx
@@ -80,102 +80,109 @@ export default function BetaSignup() {
   };
 
   return (
-    <div className="flex h-dvh w-screen items-center justify-center bg-background pt-12 md:pt-0">
-      <Card className="w-full max-w-md border-none shadow-none">
-        <CardHeader className="py-5">
-          <CardTitle className="text-center">Early Access</CardTitle>
-          <p className="text-center text-sm text-gray-500 dark:text-zinc-400">
-            Enter your email to get access once we launch ❤️
-          </p>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 uppercase">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem className="space-y-1">
-                    <FormLabel className="text-xs text-gray-600 dark:text-zinc-400">
-                      Email Address
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        placeholder="you@example.com"
-                        className="placeholder:text-sm"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="pt-2">
-                <Button type="submit" className="w-full" disabled={isSubmitting}>
-                  Join early access
-                </Button>
-              </div>
-            </form>
-
-            <div className="mt-4 flex flex-col items-center gap-4">
-              <div className="flex items-center gap-4">
-                <Link
-                  href="https://discord.gg/5nwrvt3JH2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-600 hover:text-gray-900 dark:text-zinc-400 dark:hover:text-white"
-                >
-                  <Discord className="h-6 w-6" />
-                </Link>
-                <Link
-                  href="https://github.com/nizzyabi/Mail0"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-600 hover:text-gray-900 dark:text-zinc-400 dark:hover:text-white"
-                >
-                  <GitHub className="h-5 w-5" />
-                </Link>
-                <Link
-                  href="https://x.com/NizzyABI"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-600 hover:text-gray-900 dark:text-zinc-400 dark:hover:text-white"
-                >
-                  <Twitter className="h-5 w-5" />
-                </Link>
-                <Link
-                  href="https://www.youtube.com/@NizzyABI"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-600 hover:text-gray-900 dark:text-zinc-400 dark:hover:text-white"
-                >
-                  <YouTube className="h-6 w-6" />
-                </Link>
-              </div>
-            </div>
-
-            <div className="mt-4">
+    <>
+      <div className="flex min-h-dvh w-screen flex-col items-center justify-between bg-background">
+        <div className="flex w-full flex-1 items-center justify-center">
+          <Card className="mx-4 w-full max-w-md border-none shadow-none">
+            <CardHeader className="py-5">
+              <CardTitle className="text-center">Early Access</CardTitle>
               <p className="text-center text-sm text-gray-500 dark:text-zinc-400">
-                Are you developing?{" "}
-                <Link href="/mail" className="underline">
-                  Go here
-                </Link>
+                Enter your email to get access once we launch ❤️
               </p>
-              <p className="text-center text-sm text-gray-500 dark:text-zinc-400">
-                Checkout the our{" "}
-                <Link href="/privacy" className="underline">
-                  privacy policy
-                </Link>{" "}
-                and{" "}
-                <Link href="/terms" className="underline">
-                  terms of service
-                </Link>
-              </p>
-            </div>
-          </Form>
-        </CardContent>
-      </Card>
-    </div>
+            </CardHeader>
+            <CardContent>
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 uppercase">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem className="space-y-1">
+                        <FormLabel className="text-xs text-gray-600 dark:text-zinc-400">
+                          Email Address
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="you@example.com"
+                            className="placeholder:text-sm"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="pt-2">
+                    <Button type="submit" className="w-full" disabled={isSubmitting}>
+                      Join early access
+                    </Button>
+                  </div>
+                </form>
+
+                <div className="mt-4 flex flex-col items-center gap-4">
+                  <div className="flex items-center gap-4">
+                    <Link
+                      href="https://discord.gg/5nwrvt3JH2"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-gray-600 hover:text-gray-900 dark:text-zinc-400 dark:hover:text-white"
+                    >
+                      <Discord className="h-6 w-6" />
+                    </Link>
+                    <Link
+                      href="https://github.com/nizzyabi/Mail0"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-gray-600 hover:text-gray-900 dark:text-zinc-400 dark:hover:text-white"
+                    >
+                      <GitHub className="h-5 w-5" />
+                    </Link>
+                    <Link
+                      href="https://x.com/NizzyABI"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-gray-600 hover:text-gray-900 dark:text-zinc-400 dark:hover:text-white"
+                    >
+                      <Twitter className="h-5 w-5" />
+                    </Link>
+                    <Link
+                      href="https://www.youtube.com/@NizzyABI"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-gray-600 hover:text-gray-900 dark:text-zinc-400 dark:hover:text-white"
+                    >
+                      <YouTube className="h-6 w-6" />
+                    </Link>
+                  </div>
+                </div>
+
+                <div className="mt-4">
+                  <p className="text-center text-sm text-gray-500 dark:text-zinc-400">
+                    Are you developing?{" "}
+                    <Link href="/mail" className="underline">
+                      Go here
+                    </Link>
+                  </p>
+                </div>
+              </Form>
+            </CardContent>
+          </Card>
+        </div>
+
+        <footer className="w-full">
+          <div className="container mx-auto py-4">
+            <nav className="flex justify-center space-x-6">
+              <Link href="/privacy" className="text-sm text-muted-foreground" target="_blank">
+                Privacy Policy
+              </Link>
+              <Link href="/terms" className="text-sm text-muted-foreground" target="_blank">
+                Terms of Service
+              </Link>
+            </nav>
+          </div>
+        </footer>
+      </div>
+    </>
   );
 }
