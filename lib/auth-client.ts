f0d90147dfb75bf926c3752b1116b861907e60e9
Nizzy <nizabizaher@gmail.com>
2025-02-14 02:07:36 -0500

    process env

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,10 +1,9 @@
 import { customSessionClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import type { auth } from "@/lib/auth"; // Import the auth instance as a type
-import { env } from "./env";
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_APP_URL,
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
   plugins: [customSessionClient<typeof auth>()],
 });
 
