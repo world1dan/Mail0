4f7dcf7997a0630280c078be24135d7e5a162dbf
Nizzy <nizabizaher@gmail.com>
2025-02-14 00:27:10 -0500

    homepage image

--- components/home/hero-image.tsx
+++ components/home/hero-image.tsx
@@ -5,7 +5,7 @@ const HeroImage = () => {
     <div className="mx-auto mt-8 w-full max-w-5xl overflow-hidden px-4">
       <div className="relative items-center justify-center rounded-xl border border-muted bg-[#b4b2b21a] p-1 shadow-xl shadow-black backdrop-blur-lg md:flex md:animate-move-up md:p-5">
         <Image
-          src={"/mail.png"}
+          src="/homepage-image.png"
           alt="hero"
           width={800}
           height={600}
