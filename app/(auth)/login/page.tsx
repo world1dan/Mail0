3088eca18a34159d73ac6b796a2c183927b41c81
Aman Kumar Bairagi <118182376+amanbairagi30@users.noreply.github.com>
2025-02-16 02:20:27 +0530

    feat: redesign login page (#188)
    * feat:redesign login page
    
    * fix: minor design issue
    
    * remove particle
    
    * removed unused imports

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { signIn, useSession } from "@/lib/auth-client";
+import { GitHub } from "@/components/icons/icons";
 import { Button } from "@/components/ui/button";
+import { CircleUserRound } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "sonner";
@@ -20,49 +22,51 @@ export default function Login() {
   if (isPending || (session && session.connectionId)) return null;
 
   return (
-    <div className="flex h-dvh w-screen items-center justify-center bg-background">
-      <Card className="w-full max-w-md border-none shadow-none">
-        <CardHeader className="py-5">
-          <CardTitle className="text-center">Log into your Mail0 account</CardTitle>
-          <p className="text-center text-sm text-gray-500 dark:text-zinc-400">
-            Login to your account to continue
-          </p>
-        </CardHeader>
-        <CardContent>
-          <div className="flex max-w-md justify-center gap-2">
-            <Button
-              onClick={async () => {
-                toast.promise(
-                  signIn.social({
-                    provider: "google",
-                    callbackURL: "/mail",
-                  }),
-                  {
-                    loading: "Redirecting...",
-                    success: "Redirected successfully!",
-                    error: "Login redirect failed",
-                  },
-                );
-              }}
-              variant="outline"
-              className="w-full"
-              type="button"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M11.99 13.9v-3.72h9.36c.14.63.25 1.22.25 2.05c0 5.71-3.83 9.77-9.6 9.77c-5.52 0-10-4.48-10-10S6.48 2 12 2c2.7 0 4.96.99 6.69 2.61l-2.84 2.76c-.72-.68-1.98-1.48-3.85-1.48c-3.31 0-6.01 2.75-6.01 6.12s2.7 6.12 6.01 6.12c3.83 0 5.24-2.65 5.5-4.22h-5.51z"
-                ></path>
-              </svg>
-              Google
-            </Button>
-            <Button disabled variant="outline" className="w-full" type="button">
-              <svg className="mr-2 h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-              </svg>
-              GitHub
-            </Button>
+    <div className="flex max-h-dvh min-h-screen w-screen items-center justify-center overflow-hidden border-2 bg-grid-small-black/[0.39] dark:bg-grid-small-white/[0.025]">
+      <Card className="relative z-[20] flex h-fit w-[350px] flex-col items-center justify-center overflow-hidden rounded-2xl bg-background/20 py-2 backdrop-blur-xl">
+        <CardHeader className="flex items-center justify-center">
+          <div className="mb-4 flex size-14 items-center justify-center rounded-full bg-primary/20">
+            <CircleUserRound />
           </div>
+          <CardTitle className="text-center text-lg font-normal">
+            Login into your <span className="font-bold">Mail0</span> account
+          </CardTitle>
+          <CardDescription className="text-center text-xs">
+            Login to your account to continue{" "}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="my-4 flex w-full flex-col gap-3">
+          <Button
+            onClick={async () => {
+              toast.promise(
+                signIn.social({
+                  provider: "google",
+                  callbackURL: "/mail",
+                }),
+                {
+                  loading: "Redirecting...",
+                  success: "Redirected successfully!",
+                  error: "Login redirect failed",
+                },
+              );
+            }}
+            className="h-9 w-full bg-gradient-to-b from-primary/85 via-primary to-primary/85 hover:bg-primary/40"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M11.99 13.9v-3.72h9.36c.14.63.25 1.22.25 2.05c0 5.71-3.83 9.77-9.6 9.77c-5.52 0-10-4.48-10-10S6.48 2 12 2c2.7 0 4.96.99 6.69 2.61l-2.84 2.76c-.72-.68-1.98-1.48-3.85-1.48c-3.31 0-6.01 2.75-6.01 6.12s2.7 6.12 6.01 6.12c3.83 0 5.24-2.65 5.5-4.22h-5.51z"
+              ></path>
+            </svg>
+            Log In with Google
+          </Button>
+          <Button
+            disabled
+            className="h-9 w-full bg-gradient-to-b from-primary/85 via-primary to-primary/85 hover:bg-primary/40"
+          >
+            <GitHub />
+            Log In with Github
+          </Button>
         </CardContent>
       </Card>
     </div>
