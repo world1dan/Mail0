010a570587ae66e4fe484819dded2250c69b3449
user-64bit <arth.prajapati@outlook.com>
2025-02-19 18:18:54 +0530

    fix: redirect user to /mail/inbox if already logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import HeroImage from "@/components/home/hero-image";
+import { useSession } from "@/lib/auth-client";
 import Navbar from "@/components/home/navbar";
+import { redirect } from "next/navigation";
 import Hero from "@/components/home/hero";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    redirect("/mail/inbox");
+  }
   return (
     <div className="relative h-screen min-h-screen w-full overflow-y-scroll scroll-smooth bg-grid-small-black/[0.39] dark:bg-grid-small-white/[0.025]">
       {/* Radial gradient for the container to give a faded look */}
