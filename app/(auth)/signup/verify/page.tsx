6b19197bb49723f0478051168df7bbb41765cc73
Aj Wazzan <x_1337@outlook.com>
2025-02-20 11:21:40 -0800

    feat: add Suspense to verify email page

--- app/(auth)/signup/verify/page.tsx
+++ app/(auth)/signup/verify/page.tsx
@@ -1,11 +1,14 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Mail } from "lucide-react";
+import { Suspense } from "react";
 import Link from "next/link";
 
 export default function VerifyEmail() {
   return (
     <div className="flex h-dvh w-screen flex-col items-center justify-center bg-background">
-      <Mail className="h-16 w-16 animate-pulse text-muted-foreground" />
+      <Suspense>
+        <Mail className="h-16 w-16 animate-pulse text-muted-foreground" />
+      </Suspense>
       <Card className="w-full max-w-md border-none py-0 shadow-none">
         <CardHeader className="py-2">
           <CardTitle className="text-center">Verify your email</CardTitle>
